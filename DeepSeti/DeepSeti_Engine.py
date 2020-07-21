55250b8169515ddc3aa418eb0a759ed90374547d
Peter Ma` <shadowfax0230@gmail.com>
2020-07-21 13:29:25 -0400

    docker name changes

--- DeepSeti/DeepSeti_Engine.py
+++ DeepSeti/DeepSeti_Engine.py
@@ -1,31 +1,19 @@
-from google.colab import drive
 import wget
-drive.mount('/content/drive/')
 import pandas as pd
 import requests
 from DeepSeti import DeepSeti
 import os 
 
 
-data = pd.read_csv (r'/content/drive/My Drive/Deeplearning/SETI/database.csv')
-data.columns = ['id', 'project', 'time', 'target_name', 'ra', 'decl', 'center_freq', 'file_type', 'size', 'md5sum', 'url']
+print("Running")
 
-data = data.loc[data['project'] == 'GBT']
-data = data.loc[data['file_type'] == 'HDF5']
-data.to_csv('database_gbt_h5.CSV', sep=',')
+context = zmq.Context()
+socket = context.socket(zmq.PULL)
+socket.bind("tcp://*:5555")
+broadcast = context.socket(zmq.PUB)
+broadcast.connect("tcp://10.0.3.141:5559")
 
-data_numpy = data.values[:,1:]
-
-links_list =[]
-for i in range(0,data_numpy.shape[0]): 
-  string = data_numpy[i,9]
-  string = string.replace('fine.h5','mid.h5')
-  string = string.replace('time.h5','mid.h5')
-  links_list.append(string)
-short_list = []
-
-for k in range(0,int(len(links_list)/3)):
-  short_list.append(links_list[k*3])
+print("Socket connected")
 
 
 DeepSeti = DeepSeti()
@@ -39,7 +27,7 @@ for i in range(100,200):
     DeepSeti.prediction(model_location='/content/encoder_injected_model_Cudda.h5', 
                     test_location='/content/'+file_download, 
                     anchor_location='/content/GBT_58402_66967_HIP66130_mid.h5', 
-                    top_hits=1, target_name=file_download,
+                    top_hits=100, target_name=file_download,
                     output_folder='/content/drive/My Drive/Deeplearning/SETI/output_folder/')
     os.remove('/content/'+file_download)
     print("Search Execution Complete")
