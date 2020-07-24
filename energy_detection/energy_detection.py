a57f3d6b08bb91ee8e08a940fba0630629fd4290
Shirley Wang <shirleywang57@berkeley.edu>
2020-07-24 14:37:39 -0500

    updating for threshold

--- energy_detection/energy_detection.py
+++ energy_detection/energy_detection.py
@@ -2,6 +2,7 @@ import zmq
 import time
 import os
 import wget
+import time
 from .. import upload
 
 print("Running")
@@ -16,8 +17,19 @@ print("Socket connected")
 
 while True:
     message_dict = {"done": False}
+    message_dict["algo_type"] = "Energy-Detection"
+    message_dict["start_timestamp"] = time.time()*1000
+
     request_dict = socket.recv_pyobj()
     data_url = request_dict["message"]
+    # http://blpd13.ssl.berkeley.edu/dl/GBT_58402_67632_HIP65057_fine.h5
+    temp_url = data_url
+    temp_url = temp_url.replace(".","")
+    temp_url = temp_url.replace(":","")
+    temp_url = temp_url.replace("/","")
+    temp_url = temp_url.replace("http","")
+    temp_url = temp_url.replace("h5","")
+    message_dict["url"] = temp_url
     print(f"Received request to process {data_url}")
     message_dict["message"] = f"Received request to process {data_url}"
     broadcast.send_pyobj(message_dict)
@@ -25,10 +37,11 @@ while True:
     start = time.time()
     filename = wget.download(data_url)
     obs_name = os.path.splitext(filename)[0]
+
     print(f"Downloaded observation {obs_name}")
     message_dict["message"] = f"Downloaded observation {obs_name}"
     broadcast.send_pyobj(message_dict)
-    fail = os.system(f"cd alien_hunting_algs/energy_detection && python3 preprocess_fine.py {os.path.join(os.getcwd(), filename)}")
+    fail = os.system(f"cd bl_reservoir/energy_detection && python3 preprocess_fine.py {os.path.join(os.getcwd(), filename)}")
     if fail:
         message_dict["message"] = f"Algorithm Failed, removing {obs_name}"
         broadcast.send_pyobj(message_dict)
@@ -43,6 +56,7 @@ while True:
     end = time.time()
 
     message_dict["done"] = True
+    message_dict["target"] = obs_name
     message_dict["message"] = f"Energy Detection and Result Upload finished in {end-start} seconds. Results uploaded to gs://bl-scale/{obs_name}"
     message_dict["processing_time"] = end-start
     message_dict["object_uri"] = f"gs://bl-scale/{obs_name}"
--- energy_detection/preprocess_fine.py
+++ energy_detection/preprocess_fine.py
@@ -26,7 +26,7 @@ parallel_coarse_chans = 28 # number of coarse channels operated on in parallel
 num_blocks = 308 // parallel_coarse_chans
 block_width = coarse_channel_width * parallel_coarse_chans
 save_png = False
-save_npy = True
+save_npy = False
 
 if __name__ == "__main__":
     g_start = time()
@@ -106,8 +106,8 @@ if __name__ == "__main__":
             for i in range(0, coarse_channel_width - 128, 128):
                 test_window = channel_data[:, i:i+256]
                 s, p = norm_test(test_window)
-                if s > stat_threshold:
-                    res.append([coarse_channel_width*(channel_ind) + i, s, p])
+                #if s > stat_threshold:
+                res.append([coarse_channel_width*(channel_ind) + i, s, p])
             return res
 
         start = time()
@@ -167,7 +167,7 @@ if __name__ == "__main__":
 
     full_df = pd.concat(frame_list, ignore_index=True)
     full_df.set_index("index")
-    full_df.to_pickle(out_dir + "/info_df.pkl")
+    full_df.to_pickle(out_dir + "/all_info_df.pkl")
 
     filtered_df = filter_images(full_df.reset_index(), 4)
     filtered_stack = np.array([])
