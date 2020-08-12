46e1e0b04f5eac54bf2b2822b8d76487eb22c348
FX196 <mikechen212@gmail.com>
2020-08-12 11:54:41 -0400

    update utils

--- utils.py
+++ utils.py
@@ -1,8 +1,23 @@
+standard = (
+    "energy_detection",
+)
+
+special = {
+    "turboSETI": "/code/bl_reservoir/turboSETI/turboSETI_env/bin/python3 -m turbo_seti.find_doppler.seti_event "
+}
+
+alg_working_directories = {
+    "energy_detection": "/code/bl_reservoir/energy_detection"
+}
+
+
 def get_algo_type(package_name):
     if package_name == "energy_detection":
         return "Energy-Detection"
 
 
-alg_working_directories = {
-    "energy_detection": "/code/bl_reservoir/energy_detection"
-}
+def get_algo_command_template(package_name, alg_name):
+    if package_name in standard:
+        return lambda input_file, output_file: \
+            (f'/code/bl_reservoir/{package_name}/{package_name}_env/bin/python3'
+             f' {alg_name} {input_file} {output_file}')
