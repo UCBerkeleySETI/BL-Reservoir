a97411bfe67b6555a7a84fb6ad6ea6c71bcca1cb
FX196 <mikechen212@gmail.com>
2020-08-13 01:03:34 -0400

    safe mkdir

--- server.py
+++ server.py
@@ -69,9 +69,14 @@ while True:
             logging.debug('Calling: ')
             logging.debug(get_algo_command_template(request["alg_package"], request["alg_name"])
                           (f'/code/{filename}', f'/buckets/bl-scale/{obs_name}/{request["alg_package"]}/{request["alg_name"]}').split())
-            os.mkdir(f'/buckets/bl-scale/{obs_name}')
-            os.mkdir(f'/buckets/bl-scale/{obs_name}/{request["alg_package"]}')
-            os.mkdir(f'/buckets/bl-scale/{obs_name}/{request["alg_package"]}/{request["alg_name"]}')
+            dirs = (f'/buckets/bl-scale/{obs_name}',
+                    f'/buckets/bl-scale/{obs_name}/{request["alg_package"]}',
+                    f'/buckets/bl-scale/{obs_name}/{request["alg_package"]}/{request["alg_name"]}')
+            for dir in dirs:
+                try:
+                    os.mkdir(dir)
+                except FileExistsError:
+                    continue
             fail = subprocess.call(
                 get_algo_command_template(request["alg_package"], request["alg_name"])
                 (f'/code/{filename}', f'/buckets/bl-scale/{obs_name}/{request["alg_package"]}/{request["alg_name"]}').split(), cwd=alg_workdir)
