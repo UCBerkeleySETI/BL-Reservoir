74ee54d5317119b0b1f413ab911135e2800da8bd
Peter Ma` <shadowfax0230@gmail.com>
2020-09-11 16:33:59 -0400

    MeerKAT benchmarkingconda activate Python3.6

---
+++ temp_meerKAT_analysis/resnet_50_benchmark.py
@@ -0,0 +1,91 @@
+import tensorflow as tf
+import numpy as np
+import time
+from collections import defaultdict
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
+from keras.applications import resnet50
+from keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import skimage
+from skimage import transform
+from PIL import Image
+from matplotlib import cm
+import cv2
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from matplotlib import pyplot as plt
+import random
+from tqdm import tqdm
+import time 
+
+
+start = time.time()
+print("Loading Windows Samples")
+np_images = np.load('../input/gbt-energy-detection-outputs-fine-resolution/GBT_58202_44201_KEPLER738B_fine_filtered.npy')
+np_images = np.take(np_images,np.random.permutation(np_images.shape[0]),axis=0,out=np_images)
+print(np_images.shape)
+
+print("Downloading Resnet 50 model")
+conv_only_model = ResNet50(include_top=False,
+                 weights='imagenet',
+                 input_shape=(32, 256, 3),
+                 pooling="max")
+# conv_only_model.summary()
+
+
+def resize_and_rgb(img, shape=(224, 224)):
+  np_images_resized = skimage.transform.resize(image=img, output_shape = shape)
+  np_images_resized -= np.min(np_images_resized)
+  np_images_resized = np_images_resized / np.max(np_images_resized)
+  im = cv2.cvtColor(np.float32(np_images_resized),cv2.COLOR_GRAY2RGB)
+  return im
+
+print("Resize and preprocess data.")
+converted_img = np.zeros((5000, 32, 256, 3))
+for k in tqdm(range(converted_img.shape[0])):
+  converted_img[k,:,:,:] = resize_and_rgb(np_images[k,:,:], (32, 256))
+
+# # Scale the input image to the range used in the trained network
+x = resnet50.preprocess_input(converted_img)
+# # Run the image through the deep neural network to make a prediction
+print("Pushing Data through the network")
+predictions = conv_only_model.predict(x)
+
+
+def DBSCAN_clustering_fit(inputdata):
+  dbscan = DBSCAN(eps=0.5, min_samples=3, metric='euclidean', metric_params=None, algorithm='auto', leaf_size=30, p=None, n_jobs=-1)
+  prediction = dbscan.fit_predict(inputdata)
+  print(np.max(prediction))
+  clusters = np.max(prediction)
+  print(prediction)
+  generated = np.zeros((clusters))
+  for i in range(0,inputdata.shape[0]):
+    for k in range(0,clusters):
+      if prediction[i]==k:
+        generated[k]+=1
+  print(generated)
+
+  names = np.zeros((clusters))
+  for t in range(0,clusters):
+    names[t]=t
+  plt.bar(names, generated)
+  plt.savefig('density_cluster.png')
+  return prediction, dbscan
+
+print("Density clustering...")
+dbscan_labels, dbscan = DBSCAN_clustering_fit(predictions)
+
+dbscan_cluster_members = defaultdict(list)
+for i in range(dbscan_labels.shape[0]):
+    dbscan_cluster_members[dbscan_labels[i]].append(i)
+
+# Getting outliers 
+print(np.count_nonzero(dbscan_labels == -1))
+print("Elapsed time:" + str(time.time()- start))
