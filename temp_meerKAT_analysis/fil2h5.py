a8227a49a1f5347dc7b0b3af93d185e896a767c8
Peter Ma` <shadowfax0230@gmail.com>
2020-09-12 17:24:50 -0400

    meerKAT analysis

--- temp_meerKAT_analysis/fil2h5.py
+++ temp_meerKAT_analysis/fil2h5.py
@@ -1,10 +1,7 @@
 import numpy as np
 from blimpy import Waterfall
-
 import h5py
 filename = "/data/meerKAT_data.fil"
-  
 obs = Waterfall(filename, max_load=5)
-
 with h5py.File(filename+".hdf5", "w") as data_file:
     data_file.create_dataset("data", data=obs.data)
