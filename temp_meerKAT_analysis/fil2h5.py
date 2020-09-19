af43d7f40b96246ba8d9905e2520b43f2197ad21
Peter Ma` <shadowfax0230@gmail.com>
2020-09-18 23:03:28 -0400

    h5 issues

--- temp_meerKAT_analysis/fil2h5.py
+++ temp_meerKAT_analysis/fil2h5.py
@@ -3,5 +3,5 @@ from blimpy import Waterfall
 import h5py
 filename = "data/meerKAT_data.fil"
 obs = Waterfall(filename, max_load=5)
-with h5py.File(filename+".hdf5", "w") as data_file:
+with h5py.File(filename+".h5", "w") as data_file:
     data_file.create_dataset("data", data=obs.data[:,:,:])
