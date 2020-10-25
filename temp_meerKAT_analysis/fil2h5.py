835ab14782edcbac2042b786de7e6db238d83eef
Peter Ma <45887817+PetchMa@users.noreply.github.com>
2020-10-24 20:35:40 -0400

    Update fil2h5.py

--- temp_meerKAT_analysis/fil2h5.py
+++ temp_meerKAT_analysis/fil2h5.py
@@ -2,6 +2,6 @@ import numpy as np
 from blimpy import Waterfall
 import h5py
 filename = "data/meerKAT_data.fil"
-obs = Waterfall(filename, max_load=20)
+obs = Waterfall(filename, max_load=25)
 with h5py.File(filename+".h5", "w") as data_file:
     data_file.create_dataset("data", data=obs.data[:,:,:])
