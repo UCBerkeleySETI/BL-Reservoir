22c0adbaea7a737f55e9c2aa771c9681d9cd9c1b
Peter Ma` <shadowfax0230@gmail.com>
2020-11-22 15:23:44 -0500

    Update fil2h5.py

--- temp_meerKAT_analysis/fil2h5.py
+++ temp_meerKAT_analysis/fil2h5.py
@@ -2,7 +2,6 @@ import numpy as np
 from blimpy import Waterfall
 import h5py
 import sys 
-
 filename = str(sys.argv[1])
 obs = Waterfall(filename, max_load=25)
 with h5py.File(filename+".h5", "w") as data_file:
