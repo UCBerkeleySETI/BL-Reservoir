276df85d2ee0f078a51e880d1f190ab0d0ca3eae
Peter Ma` <shadowfax0230@gmail.com>
2020-12-15 21:09:15 -0500

    distribute compute

--- temp_meerKAT_analysis/PKS_multibeam/listen.py
+++ temp_meerKAT_analysis/PKS_multibeam/listen.py
@@ -14,33 +14,34 @@ def open_csv(file):
     return file_names
 
 def difference(list_1,list_2):
-    diff = []
-    for i in list_2:
-        if i in list_1:
-            continue
-        else:
-            diff.append(i)
-    return diff
+    return list_2[len(list_1)]
 
-original_list = None
+original_list = open_csv('file_listener.csv')
 while True:
     new_list = open_csv('file_listener.csv')
-    if new_list != original_list:
-        diff = difference(original_list,new_list)
-        print("Update Detected: Executing Run "+str(diff[0]))
-        if input_line ==1:
-            file_path = '/mnt_blpd11/datax2/collate_mb/PKS_0371_2018-10-19T02:00/*/'+str(diff[0])
-            os.system("bulk_execute_1-11.py "+file_path+" "+str(diff[0]))
-        elif input_line ==2:
-            file_path = '/mnt_blpd11/datax2/collate_mb/PKS_0371_2018-10-19T02:00/*/'+str(diff[0])
-            os.system("bulk_execute_12-22.py "+file_path+" "+str(diff[0]))
-        elif input_line ==3:
-            file_path = '/mnt_blpd11/datax2/collate_mb/PKS_0371_2018-10-19T02:00/*/'+str(diff[0])
-            os.system("bulk_execute_23-32.py "+file_path+" "+str(diff[0]))
+    if len(new_list) > len(original_list):
+        diff = difference(original_list,new_list)[0]
+        print("Update Detected: Executing Run "+str(diff))
+        if int(input_line) ==1:
+            print("computing node: "+str(input_line))
+            file_path = '/mnt_blpd11/datax2/collate_mb/PKS_0371_2018-10-19T02:00/blc01/'+str(diff)
+            print("bulk_execute_1-11.py "+file_path+" "+str(diff))
+            os.system("bulk_execute_1-11.py "+file_path+" "+str(diff))
+        elif int(input_line) ==2:
+            print("computing node: "+str(input_line))
+            file_path = '/mnt_blpd11/datax2/collate_mb/PKS_0371_2018-10-19T02:00/blc12/'+str(diff)
+            print("bulk_execute_12-22.py "+file_path+" "+str(diff))
+            os.system("bulk_execute_12-22.py "+file_path+" "+str(diff))
+        elif int(input_line) ==3:
+            print("computing node: "+str(input_line))
+            file_path = '/mnt_blpd11/datax2/collate_mb/PKS_0371_2018-10-19T02:00/blc23/'+str(diff)
+            print("bulk_execute_23-32.py "+file_path+" "+str(diff))
+            os.system("bulk_execute_23-32.py "+file_path+" "+str(diff))
         else:
             print("wrong compute node")
         original_list = new_list     
     else:
-        time.sleep(20)
+        print("No Update----------------")
+        time.sleep(3)
 
 
