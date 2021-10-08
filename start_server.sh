599390a0bff0596ba087d5028c9857344ec06775
FX196 <yuhongc@berkeley.edu>
2021-10-07 17:42:08 -0700

    update remote

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,5 @@
-git fetch --all --tags --prune
+git remote add dev git@github.com:UCBerkeleySETI/BL-Reservoir.git
+git fetch --all --tags --prune dev
 git checkout tags/dev
 cd /code
 python3 -m bl_reservoir.server
