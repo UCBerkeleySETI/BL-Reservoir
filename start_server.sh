6d305bccb66ec574599185442c9f3c10451bc962
Yuhong Chen <mikechen212@gmail.com>
2021-10-14 00:12:23 -0700

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 git remote add dev git@github.com:UCBerkeleySETI/BL-Reservoir.git
-git fetch --all --tags --prune dev
+git fetch --all --tags
 git checkout tags/dev
 cd /code
 python3 -m bl_reservoir.server
